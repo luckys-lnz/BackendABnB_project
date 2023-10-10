9172a643681f9a132cc96e8c18b9156867bab164
luckys-lnx <lalucky22@gmail.com>
2023-10-10 11:32:02 -0700

    1. Be pycodestyle compliant!

---
+++ console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" The console program for the AIRBnB.
+    This program manages the Backend/control flow of the program
+"""
+
+import cmd
+
+class Console(cmd.Cmd):
+    pass
