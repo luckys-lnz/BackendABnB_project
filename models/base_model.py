8514ec7bcdddebff31bd82acb34fe003c3b60e9c
luckys-lnx <lalucky22@gmail.com>
2023-10-10 12:17:21 -0700

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -11,23 +11,26 @@ class BaseModel:
 
     def __init__(self):
         """ creating instances """
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """ returns string representation of the instances """
-        return f"[self.__class__.name] ({self.id}) ({self.__dict__})"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ updates the public instace "updte_now" with current datetime """
-        self.__dict__.update({'updated_at': datetime.now()})
+        """ updates the public instace "updated_at" with current datetime """
+        self.__dict__['updated_at'] = datetime.now()
 
     def to_dict(self):
         """ Returns keys/vals of __dict__ of the instance """
         res_dict = dict(self.__dict__)
-        res_dict.update({'__class__': self.__class__.__name__,
-                         'updated_at': self.updated_at.isoformat(),
-                         'id': self.id,
-                         'created_at': self.created_at.isoformat()})
+        res_dict.update({
+            '__class__': self.__class__.__name__,
+            'updated_at': self.updated_at.isoformat(),
+            'id': self.id,
+            'created_at': self.created_at.isoformat()
+        })
         return res_dict
+
