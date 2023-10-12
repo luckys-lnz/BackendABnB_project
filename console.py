e2d530d0a81daf3adfefc153d2ddad7f4fdfe6e7
luckys-lnx <lalucky22@gmail.com>
2023-10-12 03:40:39 -0700

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 
-""" The console program for the AIRBnB.
-    This program manages the Backend/control flow of the program
+"""
+    contains the entry point of the command interpreter
 """
 
 import cmd
 
-class Console(cmd.Cmd):
-    pass
+
+class HBNBCommand(cmd.Cmd):
+    """
+        An entry point to the command interpreter
+        which Validates the storage engine
+    """
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        """" Do nothing when empty line is executed"""
+        pass
+
+    def do_quit(self, line):
+        """ Quit Command to exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ Exit the program at the End Of File """
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
