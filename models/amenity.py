c668e6faeba001ca3ec04d47f240f75880cbf260
luckys-lnx <lalucky22@gmail.com>
2023-10-15 11:00:00 -0700

    8. First User

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines the state model
+"""
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        Definition of Amenity objects
+    """
+    name = ""
